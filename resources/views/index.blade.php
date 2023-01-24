806be1fb16026443fc286a76909a9b94d2d88981
marcopisani002 <pisanimarco34@gmail.com>
2023-01-24 15:17:07 +0100

    end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <title>FILM</title>
+    <title>TRENI</title>
 
     {{-- Viene caricato il JS ed anche il file app.scss --}}
     @vite('resources/js/app.js')
